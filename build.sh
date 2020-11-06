e0911d358642dd4d45f8807b8966871844e56bdc
David Robert-Ansart <david.robertansart@gmail.com>
2020-11-06 23:45:59 +0100

    :sparkles: Add continuous deployment bis

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 docker build . -t clap_image
-docker save clap_image | ssh -C admin@ec2-15-237-33-39.eu-west-3.compute.amazonaws.com sudo docker load
+docker save clap_image | ssh -C -o "StrictHostKeyChecking no" admin@ec2-15-237-33-39.eu-west-3.compute.amazonaws.com sudo docker load
+ssh -o "StrictHostKeyChecking no" admin@ec2-15-237-33-39.eu-west-3.compute.amazonaws.com bash ./deploy.sh
