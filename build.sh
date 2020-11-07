d84d37beef1ecf30b1cb72ed49987d65f4347132
David Robert-Ansart <david.robertansart@gmail.com>
2020-11-07 18:48:11 +0100

    try deploy 4

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 # docker build . -t clap_image
 # docker save clap_image | ssh -C -i $SSH_KEY_PATH $server sudo docker load
 # ssh -i $SSH_KEY_PATH $server 'bash -s' < deploy.sh
-ssh -i $SSH_KEY_PATH $server echo 'hello, world!'
+ssh -i $SSH_KEY_PATH $server sudo docker ps
