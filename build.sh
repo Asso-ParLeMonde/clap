65271581309277635afbc1e14b5d27dd36e572ff
David Robert-Ansart <david.robertansart@gmail.com>
2020-11-07 12:25:47 +0100

    use env var

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker build . -t clap_image
-docker save clap_image | ssh -C -o "StrictHostKeyChecking no" admin@ec2-15-237-33-39.eu-
-west-3.compute.amazonaws.com sudo docker load
-ssh -o "StrictHostKeyChecking no" admin@ec2-15-237-33-39.eu-west-3.compute.amazonaws.com 'bash -s' < deploy.sh
+docker save clap_image | ssh -C -o "StrictHostKeyChecking no" $server sudo docker load
+ssh -o "StrictHostKeyChecking no" $server 'bash -s' < deploy.sh
