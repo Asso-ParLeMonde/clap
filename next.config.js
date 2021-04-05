f32b2e19fd2c851ee6b4ef2d0d60b5258411f95c
David Robert-Ansart <david.robertansart@gmail.com>
2021-04-05 14:52:32 +0200

    :arrow_up: Update dependencies

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withPWA = require("next-pwa");
 module.exports = withPWA({
   distDir: "./dist/next",
   poweredByHeader: false,
+  future: {
+    webpack5: true,
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
